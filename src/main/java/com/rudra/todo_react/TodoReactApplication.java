d9e007a10772486bd79bfb4b30db39ed319456e9
Vipan Kumar <vipan.webdeveloper@gmail.com>
2024-11-16 18:45:27 +0530

    init'

---
+++ src/main/java/com/rudra/todo_react/TodoReactApplication.java
@@ -0,0 +1,13 @@
+package com.rudra.todo_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoReactApplication.class, args);
+	}
+
+}
